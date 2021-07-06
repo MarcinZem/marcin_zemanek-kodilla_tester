cffd6db498a795f6bdee804ac5532184e9623a99
Marcin Zemanek <marcin.zemanek99@gmail.com>
2021-07-06 15:12:45 +0200

    Initial version

--- kodilla-intro/src/main/java/HelloWorld.java
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class HelloWorld {
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Marcin");
+        System.out.println("22 lata");
+        System.out.println('z');
+    }
 }
+
--- src/main/java/Firstclass.java
+++ src/main/java/Firstclass.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class Firstclass {
+public class Firstclass public class FirstClass {                           // [1]
+    public static void main(String[] args) {         // [2]
+        System.out.println("Hello from FirstClass!"); // [3]
+    }
 }
