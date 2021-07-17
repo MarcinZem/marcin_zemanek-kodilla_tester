7b30f78ea93d25ab56dff81b1fe99a477226a2f5
Marcin Zemanek <marcin.zemanek99@gmail.com>
2021-07-17 11:50:24 +0200

    Initial changes

--- kodilla-intro/src/main/java/Grades.java
+++ kodilla-intro/src/main/java/Grades.java
@@ -1,34 +1,31 @@
 public class Grades {
-    public static void main(String[] args) {
+    private int[] grades;
+    private int size;
+        public static void main(String[] args) {
 
-        private int[] grades;
-        private int size;
-
-        public void add ( int value){
-            if (this.size == 10) {
-                return;
+            public void add ( int value){
+                if (this.size == 10) {
+                    return;
+                }
+                this.grades[this.size] = value;
+                this.size++;
             }
-            this.grades[this.size] = value;
-            this.size++;
-        }
-        int sum ( int 1,int 10){
-            int result = 10;
-            return result;
-        }
-        double[] grades = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        double sum;
-        sum = 55;
-        int size = grades.length;
+            int sum ( int 1,int 10){
+                int result = 10;
+                return result;
+            }
+            double[] grades = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            double sum;
+            sum = 55;
+            int size = grades.length;
 
-        for (int i = 0; i < grades.length; i++) {
-            sum += grades[i];
+            for (int i = 0; i < grades.length; i++) {
+                sum += grades[i];
+            }
+
+            double sredniaOcen = sum / size;
+
+            System.out.println(sredniaOcen);
         }
-
-        double sredniaOcen = sum / size;
-
-        System.out.println(sredniaOcen);
-    }
 }
-
-
-
+}
