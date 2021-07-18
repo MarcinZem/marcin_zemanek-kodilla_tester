cfe635623b0203cd28c6c56dcc0aecf0173e607e
Marcin Zemanek <marcin.zemanek99@gmail.com>
2021-07-18 16:06:40 +0200

    Initial changes

---
+++ kodilla-intro/src/main/java/User.java
@@ -0,0 +1,21 @@
+public class User {
+
+    String name;
+    double age;
+    double height;
+
+    void Validate(String name, double age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+
+    }
+}
+
+
+
+
+
+
+
+
