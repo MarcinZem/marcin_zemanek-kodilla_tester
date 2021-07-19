0ae5bc0d8459966a011f7d3ec6261928527d57f1
Marcin Zemanek <marcin.zemanek99@gmail.com>
2021-07-19 21:19:47 +0200

    initial changes

--- kodilla-intro/src/main/java/Book.java
+++ kodilla-intro/src/main/java/Book.java
@@ -1,19 +1,22 @@
 public class Book {
 
-    private String author;
-    private String title;
+    private String author = "Henryk Sienkiewicz";
+    private String title = "W pustyni i w puszczy";
+
 
     public Book(String author, String title) {
+        this.author = author;
+        this.title = title;
     }
 
-    public static void main(String[] args) {
-        System.out.println("Henryk Sienkiewicz, W pustyni i w puszczy");
+    public void main(String[] args) {
+        System.out.println(this.author, this.title);
     }
 
     private static Book of(String author, String title) {
 
 
-        Book book = Book.of("Henryk Sienkiewicz", "W pustyni i w puszczy");
+        Book book = Book.of(this.author, this.title);
 
         return book;
 
