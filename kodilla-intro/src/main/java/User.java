378f75465c3ff60cdbc75ef5828345c4dc43a780
Marcin Zemanek <marcin.zemanek99@gmail.com>
2021-07-19 21:02:42 +0200

    Initial changes

--- kodilla-intro/src/main/java/User.java
+++ kodilla-intro/src/main/java/User.java
@@ -4,12 +4,17 @@ public class User {
     double age;
     double height;
 
-    void Validate(String name, double age, double height) {
+    public User(String name, double age, double height) {
+
         this.name = name;
         this.age = age;
         this.height = height;
-
     }
+
+    void Validate
+            this.name = "Adam";
+            this.age = 40.5;
+            this.height = 178;
 }
 
 
