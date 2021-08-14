c2691d671c4496b9fee8b218f93894ccf3d6143f
Marcin Zemanek <marcin.zemanek99@gmail.com>
2021-08-14 20:40:14 +0200

    initial changes:

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/homework/SchoolDirectory.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/homework/SchoolDirectory.java
@@ -5,25 +5,24 @@ import java.util.Map;
 
 public class SchoolDirectory {
     public static void main(String[] args) {
-        Map<School, Principal> classRoom = new HashMap<>();
-        Principal oxford = new Principal("mr. Patten");
-        Principal harvard = new Principal("mr. Bacow");
-        Principal stanford = new Principal("mr. Hennessy");
+        Map<Principal, School> classRoom = new HashMap<>();
+        Principal oxford = new Principal("mr. Patten, Oxford");
+        Principal harvard = new Principal("mr. Bacow, Harvard");
+        Principal stanford = new Principal("mr. Hennessy, Stanford");
 
         School oxfordSchool = new School(21500);
         School harvardSchool = new School(25000);
         School stanfordSchool = new School(23000);
 
-        classRoom.put(oxfordSchool, oxford);
-        classRoom.put(harvardSchool, harvard);
-        classRoom.put(stanfordSchool, stanford);
+        classRoom.put(oxford, oxfordSchool);
+        classRoom.put(harvard, harvardSchool);
+        classRoom.put(stanford, stanfordSchool);
 
         System.out.println(classRoom.get(oxford));
 
-        for (Map.Entry<School, Principal> SchoolEntry : classRoom.entrySet())
-            System.out.println(SchoolEntry.getKey().getAverage());
-        for (Map.Entry<School, Principal> PrincipalEntry : classRoom.entrySet())
-            System.out.println(PrincipalEntry);
-
+        for (Map.Entry<Principal, School> SchoolEntry : classRoom.entrySet())
+            System.out.println(SchoolEntry.getKey().getFirstname());
+        for (Map.Entry<Principal, School> SchoolEntry : classRoom.entrySet())
+            System.out.println(SchoolEntry.getValue().getAverage());
     }
 }
